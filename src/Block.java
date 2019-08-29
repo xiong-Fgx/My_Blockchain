d18ee94c353ca04ce27c27cd1f0135f3e7d45a06
PowderSnow <522403969@qq.com>
2019-08-29 15:56:23 +0800

    update block chain

--- src/Block.java
+++ src/Block.java
@@ -6,12 +6,18 @@ public class Block {
      * String previousHash： 前一区块的哈希值
      * String data: 表示该区块中存的内容
      * long timeStamp: 时间戳
+     * int nonce: 用来挖矿的一个随机数，每次生成哈希值的时候，这个值都是不同的，从而使得生成的哈希值有变化
      */
     public String hash;
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
 
+    /* 构造函数
+    * 使用这个函数，将需要写的内容data和前一个区块的哈希值一同传给这个构造函数，
+    * 将区块链中的一个区块给构造出来。
+    * */
     public Block (String data, String previousHash){
         this.data = data;
         this.previousHash = previousHash;
@@ -19,12 +25,26 @@ public class Block {
         this.hash = calculateHash();
     }
 
+    /* 计算哈希值
+    * 需要根据上一个区块的哈希值+时间戳+当前区块的数据来计算出一个SHA256哈希值
+    * */
     public String calculateHash() {
-        /* 计算哈希值
-        * 需要根据上一个区块的哈希值+时间戳+当前区块的数据来计算出一个SHA256哈希值
-        * */
-        String needHash = previousHash + Long.toString(timeStamp) + data;
+        String needHash = previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data;
         String calculatedHash = util.GetHash.getHashSHA256(needHash);
         return calculatedHash;
+    }
+
+    /* 挖矿
+    * 挖矿的含义就是提供一个挖矿的难度变量difficulty，表示生成的哈希值前面有多少位为0
+    * 挖矿的方法就是不断的生成哈希值，因为SHA-256算法中有随机数，因此每次生成的哈希值都是不相同的，
+    * 只有满足前difficulty位都是0，才象征着挖矿结束，得到一个合理的区块。
+    * */
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0,difficulty).equals(target)){
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block mined! : " + hash);
     }
 }
--- src/MyBlockchain.java
+++ src/MyBlockchain.java
@@ -1,14 +1,65 @@
 import com.google.gson.GsonBuilder;
+import util.GetHash;
+
 import java.util.ArrayList;
 
 public class MyBlockchain {
     public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 4;
+
+    /*用于判断当前的区块链是否有异常
+    * 使用从头到尾的遍历，依次判断每个节点是否合法
+    * */
+    public static boolean IsValid() {
+        Block previousBlock;
+        Block currentBlock;
+        for(int i = 1; i < blockchain.size(); i++) {
+            previousBlock = blockchain.get(i-1);
+            currentBlock = blockchain.get(i);
+            String mineTarget = new String(new char[difficulty]).replace('\0', '0');
+            //当前区块的哈希值与当前区块计算出来的哈希值不相等，表示异常
+            if(!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("error! current hash is wrong or data has been modified");
+                return false;
+            }
+            //前一区块的哈希值和当前区块所记录的上衣区块哈希值不相等，表示异常
+            if(!previousBlock.hash.equals(currentBlock.previousHash)){
+                System.out.println("error! current block records different hash with previous");
+                return false;
+            }
+            if(!currentBlock.hash.substring(0,difficulty).equals(mineTarget)){
+                System.out.println("error! current block has not been mined");
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
-        blockchain.add(new Block("this is the first block", "0"));
-        blockchain.add(new Block("this is the first block", blockchain.get(blockchain.size()-1).hash));
-        blockchain.add(new Block("this is the first block", blockchain.get(blockchain.size()-1).hash));
+//        /* Version  1
+//        * 下面的主函数执行流程表示使用一个gson数据结构来存储区块链，单纯的生成区块，没有挖矿这一说
+//        * */
+//        blockchain.add(new Block("this is the first block", "0"));
+//        blockchain.add(new Block("this is the first block", blockchain.get(blockchain.size()-1).hash));
+//        blockchain.add(new Block("this is the first block", blockchain.get(blockchain.size()-1).hash));
+//        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+//        System.out.println(blockchainJson);
+        blockchain.add(new Block("This is the 1st block", "0"));
+        System.out.println("mining block 1");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("This is the 2nd block", blockchain.get(0).hash));
+        System.out.println("mining block 2");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("This is the 3rd block", blockchain.get(1).hash));
+        System.out.println("mining block 3");
+        blockchain.get(2).mineBlock(difficulty);
+
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
+
     }
 }
+
+
